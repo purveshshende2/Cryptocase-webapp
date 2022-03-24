df8528e30c88325ec101dbaa7e650a6a613c9dad
rutvikbal321 <rutvikbal321@gmail.com>
2022-03-24 17:03:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Switch, Route,Routes, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+import { Exchanges, Homepage, News, Cryptocurrencies, CryptoDetails, Navbar } from './components';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='navbar'>
+        <Navbar/> 
+      </div>
+      <div className='main'>
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+                <Homepage/>
+              </Route>
+              <Route exact path="exchanges">
+                <Exchanges/>
+              </Route>
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies/>
+              </Route>
+              <Route exact path="/crypto/:coinId">
+                <CryptoDetails/>
+              </Route>
+              <Route exact path="news">
+                <News/>
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+      </div>
+      <div className='footer'>
+
+      </div>
     </div>
   );
 }
-
+//switch allow to have multiple routes
 export default App;
