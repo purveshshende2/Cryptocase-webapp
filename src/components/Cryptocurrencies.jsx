a497582ccf68594d0e521849fa4a30d92acf0b18
Purvesh Shende <purveshshende2@gmail.com>
2022-04-02 00:19:53 +0530

    modify app

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -6,7 +6,9 @@ import {Card,Row,Col,Input} from 'antd';
 import {useGetCryptosQuery} from '../services/cryptoApi';
 
 
-const Cryptocurrencies = () => {
+const Cryptocurrencies = (simplified) => {
+
+  const count = simplified ? 10 : 100;
   const{data: cryptoList,isFetching} = useGetCryptosQuery();
   const [cryptos, setCryptos] = useState(cryptoList?.data?.coins);
 
