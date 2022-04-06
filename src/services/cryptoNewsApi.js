e4da2acd6de76134974d33da5aa5e32316144427
Purvesh Shende <56558717+purveshshende2@users.noreply.github.com>
2022-04-06 12:54:19 +0530

    Update cryptoNewsApi

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -4,7 +4,7 @@ import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 const cryptoNewsHeaders = {
         'X-BingApis-SDK': 'true',
         'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com',
-        'X-RapidAPI-Key': 'cde7c00f22msh430d8d6258ca4a0p18aa1ajsnd158684348d5'
+        'X-RapidAPI-Key': ''
 }
 
 const baseUrl = 'https://bing-news-search1.p.rapidapi.com'
