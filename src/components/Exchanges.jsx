7fee981ab401767f514227d75bbc4312867af457
Purvesh Shende <purveshshende2@gmail.com>
2022-06-18 15:27:27 +0530

    bug fixes

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -4,60 +4,56 @@ import { Collapse, Row, Col, Typography, Avatar } from 'antd';
 import HTMLReactParser from 'html-react-parser';
 
 import { useGetExchangesQuery } from '../services/cryptoApi';
-import { Loader } from './Loader';
+import  Loader  from './Loader';
 
-const { Text } = Typography;
-const { Panel } = Collapse;
+const { Panel } = Collapse
+const { Text } = Typography
 
 const Exchanges = () => {
-  const { data, isFetching } = useGetExchangesQuery();
-  const exchangesList = data?.data?.exchanges;
-  const style = { background: '#0092ff', padding: '8px 0' };
-  if (isFetching) return <Loader />;
+    const { data, isFetcing } = useGetExchangesQuery()
+    // console.log(data)
+    // console.log(data?.data?.exchanges)
+    const exchangesList = data?.data?.exchanges
+    if (isFetcing) return <Loader />
 
-  return (
-    <>
-      <Row>
-        <Col span={6}>Exchanges</Col>
-        <Col span={6}>24h Trade Volume</Col>
-        <Col span={6}>Markets</Col>
-        <Col span={6}>Change</Col>
-      </Row>
-      <Row>
-        {exchangesList.map((exchange) => (
-          <Col span={24}>
-            <Collapse>
-              <Panel
-                key={exchange.id}
-                showArrow={false}
-                header={
-                  <Row key={exchange.id} className="header-row">
-                    <Col type="flex" span={6}>
-                      <Text>
-                        <strong>{exchange.rank}.</strong>
-                      </Text>
-                      <Avatar
-                        style={{ margin: '0 10px' }}
-                        src={exchange.iconUrl}
-                      />
-                      <Text>
-                        <strong>{exchange.name}</strong>
-                      </Text>
+    return (
+        <>
+            <Row>
+                <Col span={6}>Exchanges</Col>
+                <Col span={6}>24h Trade Volume</Col>
+                <Col span={6}>Markets</Col>
+                <Col span={6}>Change</Col>
+            </Row>
+            <Row>
+                {exchangesList && exchangesList.map(exchange => (
+                    <Col span={24}>
+                        <Collapse accordion>
+                            <Panel
+                                key={exchange?.id}
+                                showArrow={false}
+                                header={(
+                                    <Row key={exchange?.id} className='header-row'>
+                                        <Col span={6}>
+                                            <Text><strong>{exchange?.rank}.</strong></Text>
+                                            <Avatar className="exchange-image" src={exchange?.iconUrl} />
+                                            <Text><strong>{exchange?.name}</strong></Text>
+                                        </Col>
+                                        <Col span={6}>${millify(exchange?.volume)}</Col>
+                                        <Col span={6}>{millify(exchange?.numberOfMarkets)}</Col>
+                                        <Col span={6}>{millify(exchange?.marketShare)}%</Col>
+                                    </Row>
+                                )}
+                            >
+                                {HTMLReactParser(exchange?.description && exchange?.description || '')}
+                            </Panel>
+
+                        </Collapse>
                     </Col>
-                    <Col span={6}>${millify(exchange.volume)}</Col>
-                    <Col span={6}>{millify(exchange.numberOfMarkets)}</Col>
-                    <Col span={6}>{millify(exchange.marketShare)}%</Col>
-                  </Row>
-                }
-              >
-                {HTMLReactParser(exchange.description || '')}
-              </Panel>
-            </Collapse>
-          </Col>
-        ))}
-      </Row>
-    </>
-  );
-};
+                ))}
+            </Row>
+        </>
+    )
+}
+
 
 export default Exchanges;
